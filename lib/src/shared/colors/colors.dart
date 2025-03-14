8487c007d087ef316e9e2de18bf612f95f92a2e0
Luiz Henrique Gomes <luiz.hgsilva@hotmail.com>
2025-03-14 15:41:35 -0300

    colors file added

---
+++ lib/src/shared/colors/colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryColor = Color.fromARGB(255, 255, 0, 0);
+  static const Color secondaryColor = Color.fromARGB(255, 255, 205, 5);
+  static const Color tertiaryColor = Color.fromARGB(255, 60, 90, 166);
+  static const Color backgroundColor = Color(0xFFF5F5F5);
+  static const Color textColor = Color(0xFF000000);
+  static const Color accentColor = Color(0xFFFF5722);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color black = Color(0xFF000000);
+  static const Color grey = Color(0xFF9E9E9E);
+}
