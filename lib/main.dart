3023556c4dd2025bbf8c71a1f054e4b54d8c933f
Luiz Henrique Gomes <luiz.hgsilva@hotmail.com>
2025-03-14 10:47:12 -0300

    firebase added to project

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,15 @@ import 'package:flutter_modular/flutter_modular.dart';
 import 'package:pokedex/src/app_module.dart';
 import 'package:pokedex/src/app_widget.dart';
 import 'package:pokedex/src/home/presenter/pages/home_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(
     ModularApp(
       module: AppModule(),
