0659efd0556edb779b76e9ff9b0c1ffec443f4f6
Luiz Henrique Gomes <luiz.hgsilva@hotmail.com>
2025-03-14 15:41:46 -0300

    outlined text widget added

---
+++ lib/src/shared/widgets/outlined_text/outlined_text_widget.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:pokedex/src/shared/colors/colors.dart';
+
+class OutlinedTextWidget extends StatelessWidget {
+  final String text;
+  final double fontSize;
+  final Color textColor;
+  final Color outlineColor;
+  final double outlineWidth;
+
+  const OutlinedTextWidget({
+    super.key,
+    required this.text,
+    this.fontSize = 60,
+    this.textColor = AppColors.secondaryColor,
+    this.outlineColor = AppColors.tertiaryColor,
+    this.outlineWidth = 10.0,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        // Texto com a cor do contorno
+        Text(
+          text,
+          style: TextStyle(
+            fontSize: fontSize,
+            fontFamily: 'PokemonFont',
+            foreground: Paint()
+              ..style = PaintingStyle.stroke
+              ..strokeWidth = outlineWidth
+              ..color = outlineColor, // Cor do contorno
+          ),
+        ),
+        // Texto com a cor normal
+        Text(
+          text,
+          style: TextStyle(
+            fontFamily: 'PokemonFont',
+            fontSize: fontSize,
+            color: textColor, // Cor do texto
+          ),
+        ),
+      ],
+    );
+  }
+}
